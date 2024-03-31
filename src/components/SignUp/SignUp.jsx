4b3b1a61bffa997121da3c424f1cebb9c1a3cd36
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2024-03-31 17:06:37 +0600

    signUp page logic added

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from '../../components/Firebase/firebase.init'
 
 function SignUp() {
-
+  const [errorMsg, seterrorMsg]= useState('')
+  const [submitMsg, setSubmitMsg]= useState('')
+  const [showPass, setshowPass]= useState(false)
 
   const handleSignUp = e => {
     e.preventDefault();
     const email = e.target.email.value
     const password = e.target.password.value
-    console.log('form Submited!', email, password);
+
+    // Reset message
+    seterrorMsg('')
+    setSubmitMsg('')
+
+    if (password.length < 6) {
+      seterrorMsg('Please enter a password of at least 6 characters')
+      return
+    }
+
+
 
     createUserWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
-    console.log(user);
+    setSubmitMsg('Form Submited!');
+
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    console.log(errorMessage);
+    seterrorMsg(errorMessage);
   });
   };
   return (
@@ -29,20 +42,23 @@ function SignUp() {
       <div className="text-center text-3xl my-6">Sign Up Now</div>
 
       <div className="mx-auto">
-        <form onSubmit={handleSignUp} className=" w-full h-full text-center">
+        <form onSubmit={handleSignUp} className=" w-full h-full mx-auto container">
           <input
             type="email"
             name="email"
             className="mb-5 py-4 w-1/3 rounded"
             placeholder=" Enter your email"
+            required
           />
           <br />
           <input
-            type="password"
+            type={ showPass ? 'text' : 'password'}
             name="password"
             className="mb-5 py-4 w-1/3 rounded"
             placeholder=" Enter your new password"
+            required
           />
+          <span className="cursor-pointer" onClick={()=> setshowPass(!showPass)}>show pass</span>
           <br />
           <input
             type="submit"
@@ -50,6 +66,12 @@ function SignUp() {
             className="mb-5 py-4 w-1/3 rounded btn btn-success"
           />
         </form>
+        {
+          errorMsg && <p className="text-red-600 text-center">{errorMsg}</p>
+        }
+        {
+          submitMsg && <p className="text-green-600 text-center">{submitMsg}</p>
+        }
       </div>
       <h1 className="text-center text-xl mt-6">
         Already have an account?{" "}
